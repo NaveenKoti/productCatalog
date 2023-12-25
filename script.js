fa5004d26a54d01b1c0bb6fd497801c8fbc13724
Naveen Koti <naveenkoti@gmail.com>
2023-12-25 16:25:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+var productData = {
+    "products": [{
+            "productId": "1",
+            "name": "Straight Thru Couplings",
+            "description": "Due to our immense capability in this work domain, we have brought forth a huge variety of Brass Straight Through Coupling.",
+            "category": "Hydraulic Couplings",
+            "features": ["Perfect finish", "Industry proven designs", "Longer life", "Optimum quality"],
+            "application": "Perfect finish, Industry proven designs, Longer life, Optimum quality",
+            "seo_keywords": ["Perfect finish, Industry proven designs, Longer life, Optimum quality"],
+            "pricing": 100,
+            "series": "STRAIGHT THROUGH (ST10)",
+            "size": "1/4” - 3”",
+            "material": ["C.S", "Brass", "SS 304", "SS 316"],
+            "seals": "Nitrile",
+            "lockingMechanism": "Ball Lock",
+            "endFittings": ["BSP Female Threads", "BSP Male Threads", "Hose Shank"],
+            "workingPressure": "1000 PSI",
+            "images": [
+                "https://4.imimg.com/data4/QK/RO/MY-1075929/industrial-straight-through-coupling-500x500.jpg",
+                "https://4.imimg.com/data4/SK/EJ/MY-1075929/straight-through-coupling-125x125.png"
+            ]
+        },
+        {
+            "productId": "2",
+            "name": "Thru Straight Couplings",
+            "description": "Due to our immense capability in this work domain, we have brought forth a huge variety of Brass Straight Through Coupling.",
+            "category": "Hydraulic Couplings",
+            "features": ["Perfect finish", "Industry proven designs", "Longer life", "Optimum quality"],
+            "application": "Perfect finish, Industry proven designs, Longer life, Optimum quality",
+            "seo_keywords": ["Industry proven designs, Perfect finish, Longer life, Optimum quality"],
+            "pricing": 100,
+            "series": "STRAIGHT THROUGH (ST10)",
+            "size": "1/4” - 3”",
+            "material": ["C.S", "Brass", "SS 304", "SS 316"],
+            "seals": "Nitrile",
+            "lockingMechanism": "Ball Lock",
+            "endFittings": ["BSP Female Threads", "BSP Male Threads", "Hose Shank"],
+            "workingPressure": "1000 PSI",
+            "images": [
+                "https://4.imimg.com/data4/SK/EJ/MY-1075929/straight-through-coupling-125x125.png",
+                "https://4.imimg.com/data4/QK/RO/MY-1075929/industrial-straight-through-coupling-500x500.jpg",
+
+            ]
+        }
+    ]
+};
